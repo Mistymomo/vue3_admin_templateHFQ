712daa62a1c1115e23e1b3e41653dabaea1efd12
momo <13072591239@163.com>
2023-10-13 08:29:00 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>我是App.vue</h1>
+    <p>{{ a }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const a = ref('hhh')
+</script>
+
+<style lang="scss" scoped>
+.app {
+  height: 500px;
+  background-color: hotpink;
+  h1 {
+    color: skyblue;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const fun = () => {
+  console.log('我爱你中国')
+}
+
+fun()
+
+const a = '你好啊，李银河'
+
+console.log(a)
